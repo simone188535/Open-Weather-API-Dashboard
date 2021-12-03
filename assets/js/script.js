800ce7e4b43bfcaad58051b660a9ed71aff4c047
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-03 18:40:13 -0500

    unneeded api call removed, html place holder removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const cityTextInputEl = document.getElementById("city-text-input");
 const submitBtnEl = document.getElementById("submit-button");
+let lat;
+let lon;
 // const displaySearchResultsEl = document.getElementById("display-search-results");
 // const currentDayForecastEl = document.getElementById('current-day-forecast');
 // const fiveDayForecastResultsEls = document.getElementsByClassName('five-day-forecast-results');
@@ -9,10 +11,6 @@ const APIKEY = '14396699e98c2a6c2a2fd6780d2fbba0';
 const BASE_URL = 'http://api.openweathermap.org/data/2.5';
 const citySearchHistory = [];
 
-function dateString(daysAdded) {
-    return moment().add(daysAdded,'days').format('M/DD/YYYY');
-}
-
 // https://stackoverflow.com/a/40981298/6195136
 // chain: https://openweathermap.org/api/one-call-api
 function getCurrentDayForcastAPIData(city) {
@@ -20,52 +18,55 @@ function getCurrentDayForcastAPIData(city) {
     .then(response => response.json())
     .then(data => {
         const { coord, name, main, wind } = data;
-        const today = dateString(0);
-        // console.log(today);
-        // console.log('currentDayForecast', data);
+        lat = coord.lat;
+        lon = coord.lon;
+        const today = moment().format('M/DD/YYYY');;
         const CDFPrimaryHeaderEl = document.querySelector('#current-day-forecast .city-name-data');
         const CDFTempDataEl = document.querySelector('#current-day-forecast .temp-data');
         const CDFWindDataEl = document.querySelector('#current-day-forecast .wind-data');
         const CDFhumidityDataEl = document.querySelector('#current-day-forecast .humidity-data');
-        // const UvhumidityDataEl = document.querySelector('#current-day-forecast .uv-data');
 
-
-        // console.log(CDFPrimaryHeaderEl);
         CDFPrimaryHeaderEl.textContent=`${name} ${today}`;
         CDFTempDataEl.textContent = `${main.temp}\u2109`;
         CDFWindDataEl.textContent = `${wind.speed} MPH`;
         CDFhumidityDataEl.textContent = `${main.humidity}\u0025`;
-        // currentDayForecastEl.style.display = 'flex';
         
-        return fetch(`${BASE_URL}/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,daily,alerts&appid=${APIKEY}`);
+        return fetch(`${BASE_URL}/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKEY}`);
     })
     .then(response => response.json())
     .then(data => {
-        const { current } = data;
-    
+
+        const { current, daily } = data;
+
+        const fiveDayForecastResultsEls = document.getElementsByClassName('five-day-forecast-results');
+        const fiveDayForecastContainer = document.getElementById('five-day-forecast-container');
         const currentDayForecastEl = document.querySelector('#current-day-forecast');
         const UvhumidityDataEl = document.querySelector('#current-day-forecast .uv-data');
 
         UvhumidityDataEl.textContent = `${current.uvi}`;
         currentDayForecastEl.style.display = 'flex';
-    })
-    .catch(function (error) {
-        alert("Unable to connect to GitHub");
-    });
-  };
 
-  function getFiveDayForcastAPIData(city) {
-    return fetch(`${BASE_URL}/forecast?q=${city}&appid=${APIKEY}&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-        const fiveDayForecastResultsEls = document.getElementsByClassName('five-day-forecast-results');
-        const fiveDayForecastCardEls = document.getElementsByClassName('five-day-forecast');
-        console.log('fiveDayForecastResultsEl', data);
 
-        // map through fiveDayForecastCardEls and api data display in Els
+        // get Five Day Forcast
+        // console.log('daily', daily);
+        const fiveDayForcast = daily.splice(1,6);
+        
+        console.log('fiveDayForcast', fiveDayForcast);
+        fiveDayForcast.forEach(element => {
+            console.log('DTTT',moment.unix(element.dt).format("MM/DD/YYYY"));
 
-        // map through fiveDayForecastResultsEls and display flex
-        for(const fiveDayForecastResultsEl of fiveDayForecastResultsEls) {
+            // append HTML block to this element fiveDayForecastResultsEls
+            const sectionEl = document.createElement('section');
+            sectionEl.setAttribute('class', 'col-lg-2 mb-3 mb-lg-0 five-day-forecast');
+            sectionEl.innerHTML = `<h2 class="my-3 mb-4 secondary-header city-name-data"></h2>
+            <p class="mb-4">Temp: <span class="temp-data"></span></p>
+            <p class="mb-4">Wind: <span class="wind-data"></span></p>
+            <p class="mb-4">Humidity: <span class="humidity-data"></span></p>`;
+            fiveDayForecastContainer.appendChild(sectionEl);
+        });
+
+        for (const fiveDayForecastResultsEl of fiveDayForecastResultsEls) {
+            // map through fiveDayForecastResultsEls and display flex
             fiveDayForecastResultsEl.style.display = 'flex';
         }
     })
@@ -80,7 +81,7 @@ function searchCity(event) {
 
     if (city) {
         getCurrentDayForcastAPIData(city);
-        getFiveDayForcastAPIData(city);
+        // getFiveDayForcastAPIData(city);
     }
 }
 
